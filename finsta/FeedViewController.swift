557e3f360c79720d259e6ee4d9938163b1cdbb40
gyul1 <gyulnarag01@gmail.com>
2019-02-16 17:55:13 -0800

    sign in and sign up

---
+++ finsta/FeedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FeedViewController.swift
+//  finsta
+//
+//  Created by Gyulnara Grigoryan  on 2/16/19.
+//  Copyright © 2019 Gyulnara Grigoryan. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ finsta/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  finsta
+//
+//  Created by Gyulnara Grigoryan  on 2/16/19.
+//  Copyright © 2019 Gyulnara Grigoryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
